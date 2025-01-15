56a42684784aa5844cb9181699328399b95b950b
Minhh199 <sminh200@gmail.com>
2025-01-15 13:09:15 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-java -Xmx2G -Xms2G -jar server.jar nogui
+java -Xmx2G -Xms8G -jar server.jar nogui
 
-java -Xmx2G -Xms2G -jar server.jar n
+java -Xmx2G -Xms8G -jar server.jar n
 
-java -Xmx2G -Xms2G -jar ser
+java -Xmx2G -Xms8G -jar ser
 
-java -Xmx2G -Xms2G -jar
+java -Xmx2G -Xms8G -jar
 
-java -Xmx2G -Xms2G 
+java -Xmx2G -Xms8G 
 
 java -Xmx2G -Xm
 
