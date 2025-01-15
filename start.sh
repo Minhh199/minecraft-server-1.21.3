c931ba25d0280efb77dae8066c4c9abfa00c8af9
Minhh199 <sminh200@gmail.com>
2025-01-15 13:08:18 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+java -Xmx2G -Xms2G -jar server.jar nogui
+
+java -Xmx2G -Xms2G -jar server.jar n
+
+java -Xmx2G -Xms2G -jar ser
+
+java -Xmx2G -Xms2G -jar
+
+java -Xmx2G -Xms2G 
+
+java -Xmx2G -Xm
+
+java -Xmx2
+
+java 
